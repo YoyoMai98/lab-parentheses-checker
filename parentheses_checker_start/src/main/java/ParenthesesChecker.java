d178afe10c4c186a6397a5f58d6545961783a4e7
Yongran <maiyongran@gmail.com>
2022-08-25 14:30:54 +0100

    comments for pseudocode

--- parentheses_checker_start/src/main/java/ParenthesesChecker.java
+++ parentheses_checker_start/src/main/java/ParenthesesChecker.java
@@ -2,5 +2,19 @@ public class ParenthesesChecker {
 
     public ParenthesesChecker (){}
 
+    public boolean checkParentheses(String stringToCheck){
+//        get the index of the first digit => int startIndex
+//        get the index of the last digit => int lastIndex
+//
+//        if startIndex digit == '('
+//          push '(' into stack, startIndex++,
+//        if lastIndex digit == ')'
+//          pop '(' out of the stack, lastIndex--
+//
+//        get subString of stringToCheck, remove the first & last digit
+//        if stringToCheck.size() == 1 or stringToCheck.isEmpty
+//          if(stack.Empty) return true
+//        else checkParentheses(stringToCheck)
+    }
 
 }
